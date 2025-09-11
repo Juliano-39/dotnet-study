230d39f15f7793f8dafef9aed39692529fa35d73
Juliano-39 <jc.pinzan39@gmail.com>
2025-09-11 11:10:12 -0300

    Criação da entidade User e aplicação de herança da entidade base

---
+++ Api_dotnet_course/src/Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    // Herda propriedades básicas da BaseEntity.
+    public class UserEntity : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
